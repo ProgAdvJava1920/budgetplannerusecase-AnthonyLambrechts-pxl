3e002907a29ef8abc42b2c06178c9579240c0616
Anthony Lambrechts <anthonylambrechts01@gmail.com>
2020-03-11 14:24:55 +0100

    Aanpassing hoofdletter package Aanpassing budgetplanner

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,18 +1,13 @@
 package be.pxl.student;
 
 import be.pxl.student.entity.Account;
-import be.pxl.student.entity.Payment;
 import be.pxl.student.util.BudgetPlannerImporter;
 
 public class BudgetPlanner {
     public static void main(String[] args) {
-        BudgetPlannerImporter importer = new BudgetPlannerImporter();
-        importer.Mapper("src\\main\\resources\\account_payments.csv");
-
-        Account account = importer.getAccount();
-        for (Payment payment : account.getPayments()) {
-            System.out.println(payment.toString());
-        }
+        String fileName = "src\\main\\resources\\account_payments.csv";
+        BudgetPlannerImporter budgetPlannerImporter = new BudgetPlannerImporter(fileName);
+        Account account = budgetPlannerImporter.deserialise();
+        System.out.println(account.toString());
     }
-
 }
